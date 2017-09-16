2fda87458c7ac2f02c95dcd53f3f7ee15e2aae2f
eytoss <jxu@dramafever.com>
2017-09-16 16:02:30 -0400

    add admin for position

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from location.models import Position
 
-# Register your models here.
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = [
+        'guid', 'position_x', 'position_y',
+        'position_orientation', 'position_accuracy',
+		'create_dt',
+    ]
+    search_fields = ['guid', 'position_x']
+    list_filter = ['position_accuracy']
+
